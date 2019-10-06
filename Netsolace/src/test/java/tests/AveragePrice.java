5e2f8a12913a158ab648505a076597278e84db83
SlavaPihidko <slava17puh@gmail.com>
2019-10-06 22:40:05 +0300

    HW. step_6

--- Netsolace/src/test/java/tests/AveragePrice.java
+++ Netsolace/src/test/java/tests/AveragePrice.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.util.List;
 
+import static org.testng.Assert.assertEquals;
+
 public class AveragePrice  extends TestBase {
 
     @Test
@@ -15,7 +17,25 @@ public class AveragePrice  extends TestBase {
         List<AvPriceFromBitcoinaverage> list = am.getApiAveragePrice().getListLast30Days();
         System.out.println("list : " + list);
 
+
         List<AvPriceFromBitcoinaverage> list2 = am.getApiAveragePrice().getList2Last30Days();
         System.out.println("list2 :" + list2);
+
+        boolean x;
+
+        for (int i=0; i <= list.size()-1; i++) {
+           double xFromList1 =  list.get(i).getAverage();
+           double xFromList2 = list2.get(i).getAverage();
+              double x1 = xFromList2 - (xFromList2/1000);
+              double x2 = xFromList2 + (xFromList2/1000);
+            System.out.println("x1 " + x1);
+            System.out.println("x2 " + x2);
+            if (x1 <= xFromList1 & xFromList1 <= x2 ){
+                x = true;
+                
+            }
+        }
+
+        assertEquals(list, list2);
     }
 }
